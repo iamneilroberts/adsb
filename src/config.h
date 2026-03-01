5ac1a0583d050c806033a57da1cb8509c38b890e
Neil Roberts <dneilroberts@gmail.com>
2026-03-01 17:03:48 -0600

    feat: increase API fetch radius to 100nm for max range level

--- src/config.h
+++ src/config.h
@@ -9,7 +9,7 @@
 #define HOME_LON -88.1632
 
 // ADS-B settings
-#define ADSB_RADIUS_NM 50
+#define ADSB_RADIUS_NM 100
 #define ADSB_POLL_INTERVAL_MS 5000
 #define MAX_AIRCRAFT 200
 #define TRAIL_LENGTH 60
